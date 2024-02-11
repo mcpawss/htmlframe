0c3874a9cd4e5220a77eb77908469bf38582f00c
mcpawss <158771577+mcpawss@users.noreply.github.com>
2024-02-11 12:43:04 +0000

    Update router.js

--- assets/router.js
+++ assets/router.js
@@ -1,4 +1,15 @@
-export default function createRouter(target1, target2) {
+import fetch from 'node-fetch';
+
+const getDailyImage = async () => {
+  const date = new Date();
+  const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 86400000);
+  const response = await fetch(`https://source.unsplash.com/collection/your_collection_id/${dayOfYear}/1600x900`);
+  return response.url;
+}
+
+export default async function createRouter(target1, target2) {
+  const randomImage = await getDailyImage();
+  const html = target1.replace("placeholder_image_url", randomImage);
   return function (req, res) {
     if (req.method === "POST") {
       const data = req.body.untrustedData
@@ -9,7 +20,7 @@ export default function createRouter(target1, target2) {
       res.statusCode = 200
       res.setHeader("Content-Type", "text/html")
       if (data.buttonIndex === 1) {
-        return res.send(target1)
+        return res.send(html)
       }
       if (data.buttonIndex === 2) {
         return res.send(target2)
