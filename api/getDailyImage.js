a021e651bbebcf5983a69f641b48b2ac49a0772a
mcpawss <158771577+mcpawss@users.noreply.github.com>
2024-02-11 12:51:59 +0000

    Update getDailyImage.js

--- api/getDailyImage.js
+++ api/getDailyImage.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch';
 const handler = async (req, res) => {
   const date = new Date();
   const dayOfYear = Math.floor((date - new Date(date.getFullYear(), 0, 0)) / 86400000);
-  const response = await fetch(`https://source.unsplash.com/collection/WM2WLKriadU/${dayOfYear}/1600x900`);
+  const response = await fetch(`https://picsum.photos/seed/${dayOfYear}/1600/900`);
   const imageUrl = response.url;
   res.status(200).json({ imageUrl });
 }
