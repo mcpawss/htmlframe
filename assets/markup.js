bd6f87aa2894f2e85a3cf52e23128348a6b4d6e4
mcpawss <158771577+mcpawss@users.noreply.github.com>
2024-02-11 12:44:16 +0000

    Update markup.js

--- assets/markup.js
+++ assets/markup.js
@@ -1,37 +1,18 @@
-export const getRandomImageMarkup = () => {
-  const images = [
-    "https://lh3.googleusercontent.com/Xtf7YrzThRSbnHqUXHAGqwaUlRQ9X2zBA8ZiIJUkpMAjFMgvA1AnBfYzsjRWgGAnUPPrHIl60iJ6iJMPz1kthCfY58euKuArT79D8dE",
-    "https://www.okx.com/cdn/nft/files/ffe6d963-f11b-4d98-950a-89ba067fde14.webp/type=detail?x-oss-process=image/resize,w_784,type_6/ignore-error,1",
-  ];
 
-  // Get the current date
-  const currentDate = new Date();
-  // Use the date to determine the index of the image to display
-  const imageIndex = currentDate.getDate() % images.length;
-
-export const getRandomImageMarkupWithScript = (imageUrl) => {
-  return `<html lang="en">
-            <head>
-              <title>Minimalist Frame - A</title>
-              <meta property="fc:frame" content="vNext" />
-              <meta
-                property="fc:frame:image"
-                content="${imageUrl}"
-              />
-              <meta
-                property="og:image"
-                content="${imageUrl}"
-                id="ogImage"
-              />
-              <script>
-                document.getElementById('ogImage').setAttribute('content', '${imageUrl}');
-              </script>
-            </head>
-            <body>
-              <h1>Minimalist Frame - A</h1>
-            </body>
-          </html>`;
-};
+export const a = `<html lang="en">
+<head>
+  <title>Minimalist Frame - A</title>
+  <meta property="fc:frame" content="vNext" />
+  <meta
+    property="fc:frame:image"
+    content="https://f4.bcbits.com/img/a0600385591_16.jpg" 
+  />
+</head>
+<body>
+  <h1>Minimalist Frame - A</h1>
+  <img id="randomImage" src="" alt="Random Image">
+</body>
+</html>`;
   
 export const b = `<html lang="en">
 <head>
